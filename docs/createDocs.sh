bab4add03892c39561b6e5e8a2c11df1f196b13c
Manuel Kiessling <kiessling@myhammer.de>
2010-05-31 14:48:43 +0200

    added useful title for docs

--- docs/createDocs.sh
+++ docs/createDocs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-phpdoc -dn PHPMergeHelper -po PHPMergeHelper -d ../MergeHelper -t ./html -o HTML:frames:phphtmllib
+phpdoc -dn PHPMergeHelper -po PHPMergeHelper -d ../MergeHelper -t ./html -o HTML:frames:phphtmllib -ti "PHPMergeHelper Documentation"
 
