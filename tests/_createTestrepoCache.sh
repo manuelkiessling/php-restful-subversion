6ebd9abd1a97bd74434ed77a5dd5a2fc2d9d9775
Manuel Kiessling <kiessling@myhammer.de>
2010-12-23 10:25:25 +0100

    old repo cache no longer needed

--- tests/_createTestrepoCache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-svn log --xml -v file:///Users/manuel/Dropbox/Projects/mergehelper/tests/_testrepo > ./_testrepocache/MergeHelper.svncache.85fea2b672885627f28d477af65ffc2221292ecb.v.x
-svn log -v file:///Users/manuel/Dropbox/Projects/mergehelper/tests/_testrepo > ./_testrepocache/MergeHelper.svncache.85fea2b672885627f28d477af65ffc2221292ecb.v
-svn log --xml file:///Users/manuel/Dropbox/Projects/mergehelper/tests/_testrepo > ./_testrepocache/MergeHelper.svncache.85fea2b672885627f28d477af65ffc2221292ecb.x
-svn log file:///Users/manuel/Dropbox/Projects/mergehelper/tests/_testrepo > ./_testrepocache/MergeHelper.svncache.85fea2b672885627f28d477af65ffc2221292ecb
-
