5f78bcebf0d3bd99752064ba2c4c4c6fdcb78229
Manuel Kiessling <kiessling@myhammer.de>
2010-12-24 08:59:33 +0100

    refactored to code style and one assert per test

--- tests/RepoPathTest.php
+++ tests/RepoPathTest.php
@@ -2,68 +2,61 @@
 
 class MergeHelper_RepoPathTest extends PHPUnit_Framework_TestCase {
 
-	public function testNormal() {
-	
+	public function test_directoryPath() {
 		$oRepoPath = new MergeHelper_RepoPath('/branches/test');
 		$this->assertSame('/branches/test', $oRepoPath->sGetAsString());
-		$this->assertSame('/branches/test', "$oRepoPath");
-		
-		$oRepoPath = new MergeHelper_RepoPath('/branches/test/a.php');
-		$this->assertSame('/branches/test/a.php', $oRepoPath->sGetAsString());
-		$this->assertSame('/branches/test/a.php', "$oRepoPath");
-
 	}
 
-	public function testExceptions() {
-	
-		$bThrown = FALSE;
-		try {
-			$oRepoPath = new MergeHelper_RepoPath('/branches/test/');
-		} catch (MergeHelper_RepoPathInvalidPathException $e) {
-			$bThrown = TRUE;
-		}
-		$this->assertTrue($bThrown);
-		
-		$bThrown = FALSE;
-		try {
-			$oRepoPath = new MergeHelper_RepoPath('branches/test');
-		} catch (MergeHelper_RepoPathInvalidPathException $e) {
-			$bThrown = TRUE;
-		}
-		$this->assertTrue($bThrown);
-		
-		$bThrown = FALSE;
-		try {
-			$oRepoPath = new MergeHelper_RepoPath('/branches/test.');
-		} catch (MergeHelper_RepoPathInvalidPathException $e) {
-			$bThrown = TRUE;
-		}
-		$this->assertTrue($bThrown);
-		
-		$bThrown = FALSE;
-		try {
-			$oRepoPath = new MergeHelper_RepoPath('/branches/test/./');
-		} catch (MergeHelper_RepoPathInvalidPathException $e) {
-			$bThrown = TRUE;
-		}
-		$this->assertTrue($bThrown);
-		
-		$bThrown = FALSE;
-		try {
-			$oRepoPath = new MergeHelper_RepoPath('/../branches/test/a.php');
-		} catch (MergeHelper_RepoPathInvalidPathException $e) {
-			$bThrown = TRUE;
-		}
-		$this->assertTrue($bThrown);
-		
-		$bThrown = FALSE;
-		try {
-			$oRepoPath = new MergeHelper_RepoPath('/branches/test/.svn');
-		} catch (MergeHelper_RepoPathInvalidPathException $e) {
-			$bThrown = TRUE;
-		}
-		$this->assertTrue($bThrown);
+	public function test_filePath() {
+		$oRepoPath = new MergeHelper_RepoPath('/branches/test/a.php');
+		$this->assertSame('/branches/test/a.php', $oRepoPath->sGetAsString());
+	}
 
+	public function test_toString() {
+		$oRepoPath = new MergeHelper_RepoPath('/branches/test/a.php');
+		$this->assertSame($oRepoPath->sGetAsString(), "$oRepoPath");
+	}
+	
+	/**
+	 * @expectedException MergeHelper_RepoPathInvalidPathException
+	 */
+	public function test_exceptionsIfEndsWithSlash() {
+		new MergeHelper_RepoPath('/branches/test/');
+	}
+
+	/**
+	 * @expectedException MergeHelper_RepoPathInvalidPathException
+	 */
+	public function test_exceptionsIfNotStartsWithSlash() {
+		new MergeHelper_RepoPath('branches/test');
+	}
+
+	/**
+	 * @expectedException MergeHelper_RepoPathInvalidPathException
+	 */
+	public function test_exceptionsIfEndsWithDot() {
+		new MergeHelper_RepoPath('/branches/test.');
+	}
+
+	/**
+	 * @expectedException MergeHelper_RepoPathInvalidPathException
+	 */
+	public function test_exceptionsIfContainsRelativePathPartsOneDot() {
+		new MergeHelper_RepoPath('/branches/test/./');
+	}
+
+	/**
+	 * @expectedException MergeHelper_RepoPathInvalidPathException
+	 */
+	public function test_exceptionsIfContainsRelativePathPartsTwoDots() {
+		new MergeHelper_RepoPath('/../branches/test/a.php');
+	}
+
+	/**
+	 * @expectedException MergeHelper_RepoPathInvalidPathException
+	 */
+	public function test_exceptionsIfContainsSvnMetadirectory() {
+		new MergeHelper_RepoPath('/branches/test/.svn');
 	}
 
 }
