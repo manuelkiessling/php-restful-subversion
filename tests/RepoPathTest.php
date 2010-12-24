f4be95b470ec49c506e6aa74496a4c6a30571cbf
Manuel Kiessling <kiessling@myhammer.de>
2010-12-24 09:29:51 +0100

    more test refactoring

--- tests/RepoPathTest.php
+++ tests/RepoPathTest.php
@@ -4,16 +4,19 @@ class MergeHelper_RepoPathTest extends PHPUnit_Framework_TestCase {
 
 	public function test_directoryPath() {
 		$oRepoPath = new MergeHelper_RepoPath('/branches/test');
+
 		$this->assertSame('/branches/test', $oRepoPath->sGetAsString());
 	}
 
 	public function test_filePath() {
 		$oRepoPath = new MergeHelper_RepoPath('/branches/test/a.php');
+
 		$this->assertSame('/branches/test/a.php', $oRepoPath->sGetAsString());
 	}
 
 	public function test_toString() {
 		$oRepoPath = new MergeHelper_RepoPath('/branches/test/a.php');
+
 		$this->assertSame($oRepoPath->sGetAsString(), "$oRepoPath");
 	}
 	
--- tests/RepoTest.php
+++ tests/RepoTest.php
@@ -2,39 +2,90 @@
 
 class MergeHelper_RepoTest extends PHPUnit_Framework_TestCase {
 
-	public function test_setRepoLocationAndPaths() {
-	
-		$oRepo = new MergeHelper_Repo();
-		$this->assertNull($oRepo->sGetLocation());
-		
-		$oRepo->setType(MergeHelper_Repo::TYPE_SVN);
-		$this->assertSame(MergeHelper_Repo::TYPE_SVN, $oRepo->iGetType());
-		
-		$oRepo->setLocation('http://svn.example.com/repo');
-		$this->assertSame('http://svn.example.com/repo', $oRepo->sGetLocation());
-		$this->assertSame('http://svn.example.com/repo/branches', $oRepo->sGetLocationBranches());
+	public function setUp() {
+		$this->oRepo = new MergeHelper_Repo();
+	}
 
-		$oRepo->setAuthinfo('user.name', 'secret');
-		$this->assertSame('user.name', $oRepo->sGetAuthinfoUsername());
-		$this->assertSame('secret', $oRepo->sGetAuthinfoPassword());
+	public function test_newRepoObjectHasNoLocationSet() {
+		$this->assertNull($this->oRepo->sGetLocation());
+	}
 
-		$oRepo->addSourcePath(new MergeHelper_RepoPath('/branches/my-hammer2/_production'), 1);
-		$oRepo->addSourcePath(new MergeHelper_RepoPath('/branches/my-hammer2/_project'), 1);
-		
-		$aoGetSourcePaths = $oRepo->aoGetSourcePaths();
-		$this->assertSame(2, sizeof($aoGetSourcePaths));
-		$this->assertEquals(new MergeHelper_RepoPath('/branches/my-hammer2/_production'), $aoGetSourcePaths[0]);
-		$this->assertEquals(new MergeHelper_RepoPath('/branches/my-hammer2/_project'), $aoGetSourcePaths[1]);
-		
-		$asGetSourceLocations = $oRepo->asGetSourceLocations();
-		$this->assertSame(2, sizeof($asGetSourceLocations));
-		$this->assertSame('http://svn.example.com/repo/branches/my-hammer2/_production', $asGetSourceLocations[0]);
-		$this->assertSame('http://svn.example.com/repo/branches/my-hammer2/_project', $asGetSourceLocations[1]);
+	public function test_setAndGetType() {
+		$this->oRepo->setType(MergeHelper_Repo::TYPE_SVN);
 
-		$oRepo->setTargetPath(new MergeHelper_RepoPath('/branches/my-hammer2/_approval'));
-		$this->assertEquals(new MergeHelper_RepoPath('/branches/my-hammer2/_approval'), $oRepo->oGetTargetPath());
-		$this->assertSame('http://svn.example.com/repo/branches/my-hammer2/_approval', $oRepo->sGetTargetLocation());
-
+		$this->assertSame(MergeHelper_Repo::TYPE_SVN, $this->oRepo->iGetType());
 	}
 	
+	public function test_setAndGetLocation() {
+		$this->oRepo->setLocation('http://svn.example.com/repo');
+
+		$this->assertSame('http://svn.example.com/repo', $this->oRepo->sGetLocation());
+	}
+
+	public function test_getLocationBranches() {
+		$this->oRepo->setLocation('http://svn.example.com/repo');
+
+		$this->assertSame('http://svn.example.com/repo/branches', $this->oRepo->sGetLocationBranches());
+	}
+
+	public function test_setAndGetAuthinfoUsername() {
+		$this->oRepo->setAuthinfo('user.name', 'secret');
+		
+		$this->assertSame('user.name', $this->oRepo->sGetAuthinfoUsername());
+	}
+
+	public function test_setAndGetAuthinfoPassword() {
+		$this->oRepo->setAuthinfo('user.name', 'secret');
+
+		$this->assertSame('secret', $this->oRepo->sGetAuthinfoPassword());
+	}
+
+	public function test_addSourcePathAndGetSourcePaths() {
+		$this->oRepo->setLocation('http://svn.example.com/repo');
+		$this->oRepo->setAuthinfo('user.name', 'secret');
+
+		$this->oRepo->addSourcePath(new MergeHelper_RepoPath('/branches/my-hammer2/_production'));
+		$this->oRepo->addSourcePath(new MergeHelper_RepoPath('/branches/my-hammer2/_project'), 1);
+
+		$aoGetSourcePaths = $this->oRepo->aoGetSourcePaths();
+
+		$this->assertEquals(array(
+		                          new MergeHelper_RepoPath('/branches/my-hammer2/_production'),
+		                          new MergeHelper_RepoPath('/branches/my-hammer2/_project')
+		                         ), $aoGetSourcePaths);
+	}
+
+	public function test_addSourcePathAndGetSourceLocations() {
+		$this->oRepo->setLocation('http://svn.example.com/repo');
+		$this->oRepo->setAuthinfo('user.name', 'secret');
+
+		$this->oRepo->addSourcePath(new MergeHelper_RepoPath('/branches/my-hammer2/_production'));
+		$this->oRepo->addSourcePath(new MergeHelper_RepoPath('/branches/my-hammer2/_project'), 1);
+
+		$asGetSourceLocations = $this->oRepo->asGetSourceLocations();
+
+		$this->assertSame(array(
+		                        'http://svn.example.com/repo/branches/my-hammer2/_production',
+		                        'http://svn.example.com/repo/branches/my-hammer2/_project'
+		                       ), $asGetSourceLocations);
+	}
+
+	public function test_setAndGetTargetPath() {
+		$this->oRepo->setLocation('http://svn.example.com/repo');
+		$this->oRepo->setAuthinfo('user.name', 'secret');
+
+		$this->oRepo->setTargetPath(new MergeHelper_RepoPath('/branches/my-hammer2/_approval'));
+
+		$this->assertEquals(new MergeHelper_RepoPath('/branches/my-hammer2/_approval'), $this->oRepo->oGetTargetPath());
+	}
+
+	public function test_setTargetPathAndGetTargetLocation() {
+		$this->oRepo->setLocation('http://svn.example.com/repo');
+		$this->oRepo->setAuthinfo('user.name', 'secret');
+
+		$this->oRepo->setTargetPath(new MergeHelper_RepoPath('/branches/my-hammer2/_approval'));
+
+		$this->assertSame('http://svn.example.com/repo/branches/my-hammer2/_approval', $this->oRepo->sGetTargetLocation());
+	}
+
 }
