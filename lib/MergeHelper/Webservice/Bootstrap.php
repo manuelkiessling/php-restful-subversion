2981801bbb8067146c13fd3099b55c5db7821b65
Manuel Kiessling <manuel@kiessling.net>
2011-03-07 21:05:08 +0000

    more movements

--- lib/MergeHelper/Webservice/Bootstrap.php
+++ lib/MergeHelper/Webservice/Bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once realpath(dirname(__FILE__)).'/../../lib/MergeHelper.php';
+require_once realpath(dirname(__FILE__)) . '/../../lib/MergeHelper.php';
 require_once 'Tonic.php';
 require_once 'ResponseHelper.php';
 require_once 'MergeHelper_Resource.php';
