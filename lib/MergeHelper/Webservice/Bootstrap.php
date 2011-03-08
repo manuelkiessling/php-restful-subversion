fea637daa2ba8c783b3198318f0195aa4c274d25
Manuel Kiessling <manuel@kiessling.net>
2011-03-08 22:07:23 +0000

    putting external libs in vendor probably is more standard

--- lib/MergeHelper/Webservice/Bootstrap.php
+++ lib/MergeHelper/Webservice/Bootstrap.php
@@ -40,7 +40,7 @@
  */
 
 require_once realpath(dirname(__FILE__).'/../../MergeHelper.php');
-require_once realpath(dirname(__FILE__).'/../../Tonic.php');
+require_once realpath(dirname(__FILE__).'/../../../vendor/Tonic.php');
 require_once realpath(dirname(__FILE__)).'/Helper/Config.php';
 require_once realpath(dirname(__FILE__)).'/Helper/Response.php';
 require_once realpath(dirname(__FILE__)).'/Resource.php';
