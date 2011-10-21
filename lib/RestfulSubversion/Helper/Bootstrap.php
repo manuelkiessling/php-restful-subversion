f17124e542d22d09e141376a7084b1aa17a5a6b1
Manuel Kiessling <manuel@kiessling.net>
2011-10-21 20:51:45 +0200

    first draft of Doctrine bootstrapping

--- lib/RestfulSubversion/Helper/Bootstrap.php
+++ lib/RestfulSubversion/Helper/Bootstrap.php
@@ -41,6 +41,10 @@
 
 namespace RestfulSubversion\Helper;
 
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+
 require_once realpath(dirname(__FILE__)) . '/Autoloader.php';
 
 spl_autoload_register('RestfulSubversion\Helper\Autoloader::load');
@@ -66,5 +70,40 @@ class Bootstrap
     public static function getLibraryRoot()
     {
         return realpath(dirname(__FILE__) . '/../');
+    }
+    
+    /**
+     * Bootstraps the Doctrine vendor library into the environment and returns an EntityManager based on the given options
+     * @param array $options
+     * @return \Doctrine\ORM\EntityManager Doctrine EntityManager
+     */
+    public function setupAndReturnDoctrineEntityManager(Array $options)
+    {
+        if (array_key_exists('libPath', $options)) {
+            $libPath = $options['libPath'];
+        } else {
+            // sensible default
+            $libPath = __DIR__ . '/../../../vendor/doctrine/lib/Doctrine/ORM/Tools/Setup.php';
+        }
+
+        require $libPath;
+        \Doctrine\ORM\Tools\Setup::registerAutoloadDirectory(__DIR__ . '/../../../vendor/doctrine/lib');
+        
+        if (array_key_exists('isDevMode', $options)) {
+            $isDevMode = $options['isDevMode'];
+        } else {
+            $isDevMode = false;
+        }
+        
+        if (!array_key_exists('connectionOptions', $options)) {
+            throw new \Exception('No database connection options provided!');
+        }
+        $connectionOptions = $options['connectionOptions'];
+        
+        $paths = array(__DIR__ . '/../Core/Entity');
+        $cache = new \Doctrine\Common\Cache\ArrayCache();
+        
+        $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, null, $cache);
+        return EntityManager::create($connectionOptions, $config);
     }
 }
